b2d56e1e596ecdbb1e36099ae425b594e236e2c7
Yoon Sejin <pentum99@gmail.com>
2025-06-04 12:21:15 +0900

    test

--- basic_syntax/basic_command.sh
+++ basic_syntax/basic_command.sh
@@ -33,3 +33,10 @@ git diff A B
 git diff commitID1 commitID2
 git diff branch1 branch2
 
+# 가장 최신의 커밋 취소
+git reset head~1
+git reset head^
+
+# 이미 push된 commit사항 되돌리기 (완전한 취소는 아님) commitID생성
+git revert 커밋ID
+
