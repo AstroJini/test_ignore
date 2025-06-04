ccd32b84865f616a0383066afcf880508fc76410
Yoon Sejin <pentum99@gmail.com>
2025-06-04 09:36:21 +0900

    메시지 제목 1. 회원가입 2. 회원 수정/삭제

--- basic_syntax/basic_command.sh
+++ basic_syntax/basic_command.sh
@@ -5,3 +5,9 @@ git status
 git add .
 # 특정파일만 add할 경우(경로까지 포함)
 git add testfolder/test1.txt
+
+
+# commit을 통해 메시지 타이틀과 메시지내용을 커밋이력으로 생성
+git commit -m "메시지제목" -m "메시지내용"
+# git commit만 입력하고 엔터시 vi모드 -> 첫줄: 타이틀, 두번째줄: 내용
+git commit
